93c7bf7ecdd1028c8fedad632ad4d90670878ecf
Jaywanth J <jaywanth.gowda11@gmail.com>
2022-11-04 23:34:59 +0530

    feat: add upload_dataset endpoint
    Added endpoint to upload files and store locally on the server

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"message": "Server is running"}
+
+
+@app.post("/upload_dataset")
+def upload_dataset(file: UploadFile):
+    accepted_mimes = ["text/csv"]
+    try:
+        contents = file.file.read()
+        print(file.content_type)
+        if file.content_type in accepted_mimes :
+            with open(file.filename, 'wb') as f:
+                f.write(contents)
+        else:
+            print("Wrong Format")
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
