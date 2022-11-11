f88a930ec401c6560ef210ea0bd52350c9eb514d
Ajay Prem <ajay01.prem@gmail.com>
2022-11-11 13:08:40 +0530

    database connection

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mbQwF71Wz3GkDNNP@db.rnitzjdeuuqtgkbjuxzp.supabase.co:5432/postgres"
 
 app = FastAPI()
-
 
 @app.get("/")
 async def read_root():
@@ -28,5 +35,28 @@ def upload_dataset(file: UploadFile):
     return {"message": f"Successfully uploaded {file.filename}"}
 
 
+@app.get("/projects")
+def fetch_projects():
+    return Base.metadata
+
+
 if __name__ == "__main__":
+    meta = MetaData()
+
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
+
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    Base = declarative_base()
+    
+    projects = Table(
+    'projects', meta, 
+    Column('id', Integer, primary_key = True), 
+    Column('name', String), 
+    )
+
+    meta.create_all(engine)
+
     uvicorn.run(app)
