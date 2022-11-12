fdf0a35639fda6558786c5ad272263c67fc20306
Jaywanth J <jaywanth.gowda11@gmail.com>
2022-11-12 10:06:38 +0000

    Add Schema for knockdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn
 import databases
 from sqlalchemy import create_engine,select
@@ -10,9 +12,28 @@ from sqlalchemy import Table, Column, Integer, String, MetaData
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 DATASET_STORAGE_PATH = "/home/athena/Desktop/ATHENA/STORAGE/DATASETS"
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://194.195.119.85:3000",
+]
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 database = databases.Database(SQLALCHEMY_DATABASE_URL)
+
+class Project(BaseModel):
+    name: str
+    description: str
+    dataset: str
 
 @app.get("/")
 async def read_root():
@@ -25,6 +46,11 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+@app.post("/knockdown")
+def knockdown(values:Project):
+    print(values)
+    return values
 
 @app.post("/upload_dataset")
 def upload_dataset(file: UploadFile):
